6f97a1bafe4270d3afa85b4c4bed6b6cf733020e
DonBeltul <150187311+DonBeltul@users.noreply.github.com>
2024-04-10 11:23:47 +0200

    Crear la clase Roca

--- modelo_lunar/wwwroot/js/lunar.ts
+++ modelo_lunar/wwwroot/js/lunar.ts
@@ -1,1 +1,13 @@
-﻿
+﻿class Roca {
+    id: string = "";
+    nombre: string = "";
+    origen: string = "";
+    dureza: number = 0;
+    tamanyo: number = 0;
+    tipo: string = "";
+    temperatura: number = 0;
+    estructura: string = "";
+    formaGrano: string = "";
+    textura: string = "";
+
+}
